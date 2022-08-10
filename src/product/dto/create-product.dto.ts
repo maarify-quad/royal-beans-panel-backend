2b27d9582c1c89a90b87aa1ec42fad644e954279
Emir Küçükosman <emirkucukosman@Emirs-MBP.home>
2022-08-10 13:33:54 +0300

    fix: create product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,27 +1,34 @@
-import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
 export class CreateProductDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsString({ message: 'Ürün adı metinsel bir değer olmalıdır' })
+  @IsNotEmpty({ message: 'Ürün adı gereklidir' })
   name: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsString({ message: 'Stok kodu metinsel bir değer olmalıdır' })
+  @IsOptional()
   stockCode: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsString({ message: 'Depo tipi metinsel bir değer olmalıdır' })
+  @IsNotEmpty({ message: 'Depo tipi gereklidir' })
   storageType: string;
 
-  @Min(0)
-  @IsNumber()
+  @Min(0, { message: "Miktar 0'dan büyük bir değer olmalıdır" })
+  @IsNumber({}, { message: 'Miktar sayı türünde olmalıdır' })
   amount: number;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsString({ message: 'Miktar birimi metinsel bir değer olmalıdır' })
+  @IsNotEmpty({ message: 'Miktarı birimi gereklidir' })
   amountUnit: string;
 
-  @Min(0)
-  @IsNumber()
+  @Min(0, { message: "Rezerve miktar 0'dan büyük bir değer olmalıdır" })
+  @IsNumber({}, { message: 'Rezerve miktar sayı türünde olmalıdır' })
+  @IsOptional()
   reservedAmount: number;
 }

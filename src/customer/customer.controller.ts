4a36b8ca378db937bc82e830ab63b4b1b337df68
Emir Küçükosman <emirkucukosman@Emirs-MBP.home>
2022-08-11 15:08:56 +0300

    feat: added create customer endpoint

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 
 // Services
 import { CustomerService } from './customer.service';
+
+// Guards
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+
+// DTOs
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customers')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getCustomers() {
     const customers = await this.customerService.findAll();
     return { customers };
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+    return await this.customerService.create(createCustomerDto);
   }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -5,6 +5,9 @@ import { Repository } from 'typeorm';
 // Entities
 import { Customer } from './entities/customer.entity';
 
+// DTOs
+import { CreateCustomerDto } from './dto/create-customer.dto';
+
 @Injectable()
 export class CustomerService {
   constructor(
@@ -14,5 +17,10 @@ export class CustomerService {
 
   async findAll(): Promise<Customer[]> {
     return await this.customerRepository.find();
+  }
+
+  async create(customer: CreateCustomerDto): Promise<Customer> {
+    const newCustomer = this.customerRepository.create(customer);
+    return await this.customerRepository.save(newCustomer);
   }
 }

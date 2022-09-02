ef16b2d1b1baee9df806c35369c08ac7fcb66003
Emir Küçükosman <emirkucukosman@Emirs-MacBook-Pro.local>
2022-09-02 14:45:36 +0300

    fix: update price list product dto

--- src/price-list-product/dto/update-price-list-product.dto.ts
+++ src/price-list-product/dto/update-price-list-product.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { IsNumber } from 'class-validator';
 import { PriceListProduct } from '../entities/price-list-product.entity';
 
-export class UpdatePriceListProductDto extends PartialType(PriceListProduct) {}
+export class UpdatePriceListProductDto extends PartialType(
+  OmitType(PriceListProduct, ['id']),
+) {
+  @IsNumber()
+  id: number;
+}

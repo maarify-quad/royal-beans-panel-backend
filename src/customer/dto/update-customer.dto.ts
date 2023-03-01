ff815b2a9067ad4dbde187a03545b77276610e8a
Emir Kucukosman <m.emirkucukosman@gmail.com>
2023-03-01 21:28:41 +0300

    feat: update customer name

--- src/customer/dto/update-customer.dto.ts
+++ src/customer/dto/update-customer.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 // DTOs
 import { CreateCustomerDto } from './create-customer.dto';
 
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
   id: string;
+
+  @IsString({ message: 'İsim metin olmalıdır' })
+  @IsNotEmpty({ message: 'İsim boş bırakılamaz' })
+  name: string;
 }
